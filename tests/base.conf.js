2e3c7d998ae02c2784508bdde598ddb6fdf09e85
Kenn Sippell <kennsippell@gmail.com>
2019-01-21 22:05:39 +0300

    Seems Selenium is losing contact with Chrome during travis run. Allow insecure localhost?

--- tests/base.conf.js
+++ tests/base.conf.js
@@ -14,7 +14,7 @@ class BaseConfig {
       capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-          args: ['--headless', '--disable-gpu', '--window-size=1024,768']
+          args: ['--headless', '--disable-gpu', '--allow-insecure-localhost', '--window-size=1024,768']
         }
       },
       beforeLaunch: function() {
